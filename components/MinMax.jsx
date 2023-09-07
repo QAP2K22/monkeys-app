84561f17fe265efb23ad416e5d53be8e99045454
Pedrinho Dev <103223807+QAP2K22@users.noreply.github.com>
2023-09-07 14:33:53 -0300

    Commit  atividade MinMax e Random

--- components/MinMax.jsx
+++ components/MinMax.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-const MinMax = (props) => {
- 
-    const {min,max} = props
- 
-    return (
-    /* <Text>{min>max?`${min} é maior que ${max}`:`${max} é maior que ${min}`}</Text> */
-
+const MinMax = ({min,max}) => {
+  return (
     <Text>{`${Math.max(min,max)} é maior que ${Math.min(min,max)}`}</Text>
   )
 }
--- components/NumeroAleatorio.jsx
+++ components/NumeroAleatorio.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Text } from 'react-native'
 
 const NumeroAleatorio = (props) => {
-    
+
     const { min, max } = props
     const minValue = Math.min(max,min)
     const maxValue = Math.max(max,min)
-
+    
     return (
         <Text>{`Número aleatório entre ${min} e ${max}: ${Math.floor(Math.random() * (maxValue-minValue)+minValue)}`}</Text>
     )
